9d2dfd28a9cb45fb9738d9aaa33da7dbc4c2c63c
meiqua <563714976@qq.com>
2018-07-30 11:36:30 +0800

    delete useless

--- build/forslam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/at/changable_ws/src/forslam
-# Build directory: /home/at/changable_ws/build/forslam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
